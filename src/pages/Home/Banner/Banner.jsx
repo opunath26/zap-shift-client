1d54eee1ca5f29e2fd3a8318cd429b8235e49dff
OpuTheCoder <artistop26@gmail.com>
2026-09-01 22:44:28 +0600

    feat(banner): optimize responsive carousel component with custom image height and smooth autoplay controls

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,28 +1,45 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"
-import bannerImg1 from '../../../assets/banner/banner1.png'
-import bannerImg2 from '../../../assets/banner/banner2.png'
-import bannerImg3 from '../../../assets/banner/banner3.png'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import bannerImg1 from '../../../assets/banner/banner1.png';
+import bannerImg2 from '../../../assets/banner/banner2.png';
+import bannerImg3 from '../../../assets/banner/banner3.png';
+
+const bannerImages = [
+  { id: 1, src: bannerImg1, alt: "Delivery Service Banner 1" },
+  { id: 2, src: bannerImg2, alt: "Delivery Service Banner 2" },
+  { id: 3, src: bannerImg3, alt: "Delivery Service Banner 3" },
+];
 
 const Banner = () => {
-    return (
-        <Carousel 
-        autoPlay={true}
-        infiniteLoop={true}
-        className='p-20'
+  return (
+    <section className="mx-auto px-4 py-6 sm:py-10 max-w-7xl">
+      <div className="shadow-lg rounded-2xl sm:rounded-3xl overflow-hidden">
+        <Carousel
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          showStatus={false}
+          interval={4000}
+          transitionTime={800}
+          swipeable={true}
+          emulateTouch={true}
+          stopOnHover={true}
         >
-                <div>
-                    <img src={bannerImg1} />
-                </div>
-                <div>
-                    <img src={bannerImg2} />
-                </div>
-                <div>
-                    <img src={bannerImg3} />
-                </div>
-            </Carousel>
-    );
+          {bannerImages.map((banner) => (
+            <div key={banner.id} className="w-full h-[250px] sm:h-[400px] md:h-[500px]">
+              <img
+                src={banner.src}
+                alt={banner.alt}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
