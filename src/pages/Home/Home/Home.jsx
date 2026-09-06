62932471be378c55b937fe14521d40eb7d1fdd29
OpuTheCoder <artistop26@gmail.com>
2026-09-06 22:33:21 +0600

    refactor(home): reorder landing page sections for better UX and conversion flow

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Brands from '../Brands/Brands';
 import Work from '../Banner/Works';
 import OurServices from '../Banner/OurServices';
-import Brands from '../Brands/Brands';
-import Reviews from '../Reviews/Reviews';
+import WhyChooseUs from '../WhyChooseUs/WhyChooseUs';
 import PricingCalculator from '../PricingCalculator/PricingCalculator';
 import StatsSection from '../StatsSection/StatsSection';
-import WhyChooseUs from '../WhyChooseUs/WhyChooseUs';
 import JoinBanner from '../JoinBanner/JoinBanner';
+import Reviews from '../Reviews/Reviews';
 import FAQ from '../FAQ/FAQ';
 import Newsletter from '../Newsletter/Newsletter';
 
 const reviewsPromise = fetch('/reviews.json').then(res => res.json());
 
-
 const Home = () => {
     return (
         <div>
             <Banner />
-            <StatsSection />
-            <PricingCalculator />
+            <Brands />
             <Work />
             <OurServices />
             <WhyChooseUs />
+            <PricingCalculator />
+            <StatsSection />
             <JoinBanner />
-            <FAQ />
-            <Brands />
             <Reviews reviewsPromise={reviewsPromise} />
+            <FAQ />
             <Newsletter />
         </div>
     );
